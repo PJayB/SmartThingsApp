01e70ed3c8100eb7dded1061d369c25e3f94629d
Pete Lewis <mygit@pjblewis.com>
2016-10-11 01:18:47 +0000

    Print GET, PUT, SERVER

--- webui/index.php
+++ webui/index.php
@@ -1,1 +1,25 @@
-<?php phpinfo(); ?>
+<?php
+
+echo "<h1>GET:</h1>\n";
+
+foreach ($_GET as $key => $value) {
+	echo $key . " =&gt; " . $value . "<br/>\n";
+}
+
+echo "<h1>PUT:</h1>\n";
+
+foreach ($_PUT as $key => $value) {
+	echo $key . " =&gt; " . $value . "<br/>\n";
+}
+
+echo "<h1>SERVER:</h1>\n";
+echo "REQUEST_URI: {$_SERVER['REQUEST_URI']}<br/>\n";
+
+foreach ($_SERVER as $key => $value) {
+	if (substr($key, 0, 5) === "HTTP_") {
+		echo $key . " =&gt; " . $value . "<br/>\n";
+	}
+}
+
+
+ ?>
